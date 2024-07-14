66cc9b3ca9b6471211d8daf3de9c83538cd1ba78
symbolic223 <danildemin00@gmail.com>
2024-07-14 17:48:05 +0700

    ok

---
+++ src/main/java/popka/gayzone/Gayzone.java
@@ -0,0 +1,40 @@
+package popka.gayzone;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gayzone extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent ev) {
+        Player player = ev.getPlayer();
+        String playerName = player.getName();
+        player.sendMessage(String.format("Прив, %s)", playerName));
+    }
+
+    @EventHandler
+    public void onPlayerDisconnect(PlayerQuitEvent ev) {
+        Player player = ev.getPlayer();
+        String playerName = player.getName();
+        double posx = player.getLocation().getX();
+        double posy = player.getLocation().getY();
+        double posz = player.getLocation().getZ();
+        System.out.println(String.format("Игрок %s вышел на координатах (%.2f, %.2f, %.2f)", playerName, posx, posy, posz));
+    }
+}
